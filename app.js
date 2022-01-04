9b912ec3933a2d279bdb902a247907607cc2f4a1
Harsh Kanodiya <72756692+Harsh3363@users.noreply.github.com>
2022-01-05 01:46:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,178 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+app.use(session({
+  secret:"our little secret.",
+  resave:false,
+  saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+const userSchema = new mongoose.Schema({
+  email:String,
+  password:String,
+  googleId: String,
+  secret: String
+});
+
+// will use passportLocalMongoose to hash & salt the password as well as to save it inside the database.
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// setting up google stratergy ->
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // getiing user info from the google's userinfo endpoint ->
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  // below findOrCreate is a npm package and not a function
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/",function(req,res){
+  res.render("home")
+});
+
+// using passport to authenticate our user using the google stratergy ->
+// below line of code created a pop up for the use to sign in ->
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }));
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
+app.get("/login",function(req,res){
+  res.render("login")
+});
+
+
+app.get("/register",function(req,res){
+  res.render("register")
+});
+
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/submit",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.get("/secrets",function(req,res){
+  User.find({"secret":{$ne:null}},function(err,foundUsers){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(foundUsers){
+        res.render("secrets",{usersWithSecrets: foundUsers});
+      }
+    }
+  })
+});
+
+app.post("/register",function(req,res){
+// below user.register came from passportLocalMongoose ->
+User.register({username:req.body.username},req.body.password,function(err,user){
+  if(err){
+    console.log(err);
+    res.redirect("/register");
+  }else{
+    // only triggered when the authentication was successfull
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/secrets");
+      // earlier we where not using secrets route as we were using secrets ejs file to get rendered but now user can directly access secrets route
+      // if they are loggrd in so need to have secrets route.
+    });
+  }
+});
+});
+
+app.post("/submit",function(req,res){
+const submittedSecret = req.body.secret;
+User.findById(req.user.id,function(err,foundUser){
+  if(err){
+    console.log(err);
+  }else{
+    if(foundUser){
+      foundUser.secret = submittedSecret;
+      foundUser.save(function(){
+        res.redirect("/secrets");
+      });
+    }
+  }
+});
+});
+
+app.post("/login",function(req,res){
+
+const user = new User({
+  username: req.body.username,
+  password: req.body.password
+});
+
+req.login(user,function(err){
+  if(err){
+    console.log(err);
+  }else{
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/secrets");
+    });
+  }
+});
+});
+app.listen(3000,function(){console.log("successfully started on port 3000")});
